355042f3e8728ac6f98915615df80f0c2b81eb91
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-04-23 22:16:09 -0500

    starting to look like what I want

--- wordspreader/main.py
+++ wordspreader/main.py
@@ -1,203 +1,193 @@
 import flet
-from app_layout import AppLayout
-from board import Board
-from data_store import DataStore
 from flet import (
-    AlertDialog,
-    AppBar,
+    Checkbox,
     Column,
-    Container,
-    ElevatedButton,
-    Icon,
+    FloatingActionButton,
+    IconButton,
+    OutlinedButton,
     Page,
-    PopupMenuButton,
-    PopupMenuItem,
-    RoundedRectangleBorder,
     Row,
-    TemplateRoute,
+    Tab,
+    Tabs,
     Text,
     TextField,
     UserControl,
-    View,
     colors,
     icons,
-    margin,
-    padding,
-    theme,
 )
-from memory_store import InMemoryStore
-from user import User
 
 
-class TrelloApp(UserControl):
-    def __init__(self, page: Page, store: DataStore):
+class Words(UserControl):
+    def __init__(self, task_name, task_status_change, task_delete):
         super().__init__()
-        self.page = page
-        self.store: DataStore = store
-        self.page.on_route_change = self.route_change
-        self.boards = self.store.get_boards()
-        self.login_profile_button = PopupMenuItem(text="Log in", on_click=self.login)
-        self.appbar_items = [
-            self.login_profile_button,
-            PopupMenuItem(),  # divider
-            PopupMenuItem(text="Settings"),
-        ]
-        self.appbar = AppBar(
-            leading=Icon(icons.GRID_GOLDENRATIO_ROUNDED),
-            leading_width=100,
-            title=Text("Trolli", font_family="Pacifico", size=32, text_align="start"),
-            center_title=False,
-            toolbar_height=75,
-            bgcolor=colors.LIGHT_BLUE_ACCENT_700,
-            actions=[
-                Container(
-                    content=PopupMenuButton(items=self.appbar_items),
-                    margin=margin.only(left=50, right=25),
-                )
-            ],
-        )
-        self.page.appbar = self.appbar
-        self.page.update()
+        self.completed = False
+        self.task_name = task_name
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
 
     def build(self):
-        self.layout = AppLayout(
-            self,
-            self.page,
-            self.store,
-            tight=True,
+        self.display_task = Checkbox(value=False, label=self.task_name, on_change=self.status_changed)
+        self.edit_name = TextField(expand=1)
+
+        self.display_view = Row(
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                self.display_task,
+                Row(
+                    spacing=0,
+                    controls=[
+                        IconButton(
+                            icon=icons.CREATE_OUTLINED,
+                            tooltip="Edit Words",
+                            on_click=self.edit_clicked,
+                        ),
+                        IconButton(
+                            icons.DELETE_OUTLINE,
+                            tooltip="Delete Words",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = Row(
+            visible=False,
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                self.edit_name,
+                IconButton(
+                    icon=icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        return Column(controls=[self.display_view, self.edit_view])
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change(self)
+
+    def delete_clicked(self, e):
+        self.task_delete(self)
+
+
+class WordSpreader(UserControl):
+    def build(self):
+        self.new_title = TextField(hint_text="Title the words.", on_submit=self.add_clicked, expand=True)
+        self.new_words = TextField(
+            hint_text="Provide the words.",
             expand=True,
-            vertical_alignment="start",
+            multiline=True,
         )
-        return self.layout
+        self.tasks = Column()
 
-    def initialize(self):
-        self.page.views.clear()
-        self.page.views.append(
-            View(
-                "/",
-                [self.appbar, self.layout],
-                padding=padding.all(0),
-                bgcolor=colors.BLUE_GREY_200,
+        self.filter = Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[Tab(text="all"), Tab(text="active"), Tab(text="completed")],
+        )
+
+        self.items_left = Text("0 items left")
+
+        # application's root control (i.e. "view") containing all other controls
+        return Column(
+            width=600,
+            controls=[
+                Row([Text(value="Words to Spread", style="headlineMedium")], alignment="center"),
+                Row(
+                    controls=[
+                        self.new_title,
+                    ]
+                ),
+                Row(
+                    controls=[
+                        self.new_words,
+                    ]
+                ),
+                Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                        Row(
+                            alignment="spaceBetween",
+                            vertical_alignment="center",
+                            controls=[
+                                self.items_left,
+                                OutlinedButton(text="Clear completed", on_click=self.clear_clicked),
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+    def add_clicked(self, e):
+        if self.new_title.value:
+            task = Words(self.new_title.value, self.task_status_change, self.task_delete)
+            self.tasks.controls.append(task)
+            self.new_title.value = ""
+            self.new_title.focus()
+            self.update()
+
+    def task_status_change(self, task):
+        self.update()
+
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+
+    def tabs_changed(self, e):
+        self.update()
+
+    def clear_clicked(self, e):
+        for task in self.tasks.controls[:]:
+            if task.completed:
+                self.task_delete(task)
+
+    def update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        count = 0
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.completed is False)
+                or (status == "completed" and task.completed)
             )
-        )
-        self.page.update()
-        # create an initial board for demonstration if no boards
-        if len(self.boards) == 0:
-            self.create_new_board("My First Board")
-        self.page.go("/")
-
-    def login(self, e):
-        def close_dlg(e):
-            if user_name.value == "" or password.value == "":
-                user_name.error_text = "Please provide username"
-                password.error_text = "Please provide password"
-                self.page.update()
-                return
-            else:
-                user = User(user_name.value, password.value)
-                if user not in self.store.get_users():
-                    self.store.add_user(user)
-                self.user = user_name.value
-                self.page.client_storage.set("current_user", user_name.value)
-
-            dialog.open = False
-            self.appbar_items[0] = PopupMenuItem(text=f"{self.page.client_storage.get('current_user')}'s Profile")
-            self.page.update()
-
-        user_name = TextField(label="User name")
-        password = TextField(label="Password", password=True)
-        dialog = AlertDialog(
-            title=Text("Please enter your login credentials"),
-            content=Column(
-                [
-                    user_name,
-                    password,
-                    ElevatedButton(text="Login", on_click=close_dlg),
-                ],
-                tight=True,
-            ),
-            on_dismiss=lambda e: print("Modal dialog dismissed!"),
-        )
-        self.page.dialog = dialog
-        dialog.open = True
-        self.page.update()
-
-    def route_change(self, e):
-        troute = TemplateRoute(self.page.route)
-        if troute.match("/"):
-            self.page.go("/boards")
-        elif troute.match("/board/:id"):
-            if int(troute.id) > len(self.store.get_boards()):
-                self.page.go("/")
-                return
-            self.layout.set_board_view(int(troute.id))
-        elif troute.match("/boards"):
-            self.layout.set_all_boards_view()
-        elif troute.match("/members"):
-            self.layout.set_members_view()
-        self.page.update()
-
-    def add_board(self, e):
-        def close_dlg(e):
-            if (hasattr(e.control, "text") and not e.control.text == "Cancel") or (
-                type(e.control) is TextField and e.control.value != ""
-            ):
-                self.create_new_board(dialog_text.value)
-            dialog.open = False
-            self.page.update()
-
-        def textfield_change(e):
-            if dialog_text.value == "":
-                create_button.disabled = True
-            else:
-                create_button.disabled = False
-            self.page.update()
-
-        dialog_text = TextField(label="New Board Name", on_submit=close_dlg, on_change=textfield_change)
-        create_button = ElevatedButton(text="Create", bgcolor=colors.BLUE_200, on_click=close_dlg, disabled=True)
-        dialog = AlertDialog(
-            title=Text("Name your new board"),
-            content=Column(
-                [
-                    dialog_text,
-                    Row(
-                        [
-                            ElevatedButton(text="Cancel", on_click=close_dlg),
-                            create_button,
-                        ],
-                        alignment="spaceBetween",
-                    ),
-                ],
-                tight=True,
-            ),
-            on_dismiss=lambda e: print("Modal dialog dismissed!"),
-        )
-        self.page.dialog = dialog
-        dialog.open = True
-        self.page.update()
-        dialog_text.focus()
-
-    def create_new_board(self, board_name):
-        new_board = Board(self, self.store, board_name)
-        self.store.add_board(new_board)
-        self.layout.hydrate_all_boards_view()
-
-    def delete_board(self, e):
-        self.store.remove_board(e.control.data)
-        self.layout.set_all_boards_view()
+            if not task.completed:
+                count += 1
+        self.items_left.value = f"{count} active item(s) left"
+        super().update()
 
 
 def main(page: Page):
-    page.title = "Flet Trello clone"
-    page.padding = 0
-    page.theme = theme.Theme(font_family="Verdana")
-    page.theme.page_transitions.windows = "cupertino"
-    page.fonts = {"Pacifico": "Pacifico-Regular.ttf"}
-    page.bgcolor = colors.BLUE_GREY_200
-    app = TrelloApp(page, InMemoryStore())
-    page.add(app)
+    page.title = "ToDo App"
+    page.horizontal_alignment = "center"
+    page.scroll = "adaptive"
     page.update()
-    app.initialize()
+
+    # create application instance
+    app = WordSpreader()
+
+    # add application's root control to the page
+    page.add(app)
 
 
-flet.app(target=main, assets_dir="../assets")
+flet.app(target=main)
