8c822a17178904a4f239c7bb8fb30a272ea6f127
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-05-11 23:03:18 -0500

    More unit tests

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -1,30 +1,38 @@
+from typing import TYPE_CHECKING
+
 import sqlalchemy.exc
-
-from pytest import raises, mark
 from hypothesis import strategies as st, given
+from pytest import raises, mark
+
+if TYPE_CHECKING:
+    from wordspreader.persistence import DBPersistence, Word
 
 
-def check_word(name, content, word):
+def check_word(name: str, content: str, word: "Word"):
     assert word.name == name
     assert word.content == content
 
 
-def make_get_check(name, content, db):
+# noinspection PyProtectedMember
+def make_get_check(name: str, content: str, db: "DBPersistence"):
     db.new_word(name, content)
     word = db._get_word(name)
     check_word(name, content, word)
 
 
 @given(name=st.text(max_size=100), content=st.text(max_size=300))
-def test_new_word_and_get_word(name, content, db_factory):
-    db = db_factory()
+def test_new_word_and_get_word(name: str, content: str, db_factory: callable):
+    db: "DBPersistence" = db_factory()
     make_get_check(name, content, db)
 
 
 @given(name=st.text(max_size=100), content=st.text(max_size=300))
-def test_delete_word(name, content, db_factory):
-    db = db_factory()
+def test_delete_word(name: str, content: str, db_factory: callable):
+    db: "DBPersistence" = db_factory()
     make_get_check(name, content, db)
+    db.delete_word(name)
+    with raises(sqlalchemy.exc.NoResultFound):
+        db._get_word(name)
 
 
 @mark.skip(reason='Not using tags yet')
@@ -36,9 +44,9 @@ def test_get_words_filtered():
     names=st.lists(st.text(min_size=1, max_size=100), unique=True, min_size=2, max_size=2),
     content=st.text(max_size=300),
 )
-def test__rename_word(names, content, db_factory):
+def test__rename_word(names: list[str, str], content: str, db_factory: callable):
     name, new_name = names
-    db = db_factory()
+    db: "DBPersistence" = db_factory()
     make_get_check(name, content, db)
     db._rename_word(name, new_name)
     check_word(new_name, content, db._get_word(new_name))
@@ -52,7 +60,7 @@ def test__rename_word(names, content, db_factory):
 )
 def test__update_word(name, contents, db_factory):
     content, new_content = contents
-    db = db_factory()
+    db: "DBPersistence" = db_factory()
     make_get_check(name, content, db)
     db._update_word(name=name, content=new_content)
     check_word(name, new_content, db._get_word(name))
