a9554d681f1c70546b0dd5e8f914082695facadb
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-05-11 22:53:49 -0500

    ore unit tests

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -1,4 +1,5 @@
 import sqlalchemy.exc
+from _pytest import mark
 from pytest import raises
 from hypothesis import strategies as st, given
 
@@ -8,18 +9,25 @@ def check_word(name, content, word):
     assert word.content == content
 
 
-@given(name=st.text(max_size=100), content=st.text(max_size=300))
-def test_new_word(name, content, db_factory):
-    db = db_factory()
+def make_get_check(name, content, db):
     db.new_word(name, content)
     word = db._get_word(name)
     check_word(name, content, word)
 
 
-def test_delete_word():
-    raise AssertionError()
+@given(name=st.text(max_size=100), content=st.text(max_size=300))
+def test_new_word_and_get_word(name, content, db_factory):
+    db = db_factory()
+    make_get_check(name, content, db)
 
 
+@given(name=st.text(max_size=100), content=st.text(max_size=300))
+def test_delete_word(name, content, db_factory):
+    db = db_factory()
+    make_get_check(name, content, db)
+
+
+@mark.skip(reason='Not using tags yet')
 def test_get_words_filtered():
     raise AssertionError()
 
@@ -31,25 +39,20 @@ def test_get_words_filtered():
 def test__rename_word(names, content, db_factory):
     name, new_name = names
     db = db_factory()
-    db.new_word(name, content)
-    check_word(name, content, db._get_word(name))
+    make_get_check(name, content, db)
     db._rename_word(name, new_name)
     check_word(new_name, content, db._get_word(new_name))
     with raises(sqlalchemy.exc.NoResultFound):
         check_word(name, content, db._get_word(name))
 
 
-def test__update_word():
-    raise AssertionError()
-
-
-def test__get_word():
-    raise AssertionError()
-
-
-def test__get_session():
-    raise AssertionError()
-
-
-def test__create_dbs():
-    raise AssertionError()
+@given(
+    name=st.text(max_size=100, min_size=1),
+    contents=st.lists(st.text(min_size=1, max_size=300), unique=True, min_size=2, max_size=2),
+)
+def test__update_word(name, contents, db_factory):
+    content, new_content = contents
+    db = db_factory()
+    make_get_check(name, content, db)
+    db._update_word(name=name, content=new_content)
+    check_word(name, new_content, db._get_word(name))
