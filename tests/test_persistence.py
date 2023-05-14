c4db7a91e78f887b82ae1fcb0432b98c66ad3020
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-05-14 15:07:46 -0500

    use sets, fix types

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -10,32 +10,32 @@ if TYPE_CHECKING:
 st_name = st.text(max_size=100, min_size=2)
 st_content = st.text(max_size=300)
 st_tag = st.text(max_size=20, min_size=2)
-st_tags = st.lists(st_tag, max_size=10, unique=True)
+st_tags = st.sets(st_tag, max_size=10)
 st_name_list = st.lists(st_name, min_size=2, max_size=2, unique=True)
 st_content_list = st.lists(st_content, min_size=2, max_size=2, unique=True)
 
 
-def check_word(name: str, content: str, tags: list[str], word: "Word"):
+def check_word(name: str, content: str, tags: set[str], word: "Word"):
     assert word.name == name
     assert word.content == content
     assert set(t for t in tags) == set(t.name for t in word.tags)
 
 
 # noinspection PyProtectedMember
-def make_get_check(name: str, content: str, tags: list[str], db: "DBPersistence"):
+def make_get_check(name: str, content: str, tags: set[str], db: "DBPersistence"):
     db.new_word(name, content, tags)
     word = db.get_word(name)
     check_word(name, content, tags, word)
 
 
 @given(name=st_name, content=st_content, tags=st_tags)
-def test_new_word_and_get_word(name: str, content: str, tags: list[str], db_factory: callable):
+def test_new_word_and_get_word(name: str, content: str, tags: set[str], db_factory: callable):
     db: "DBPersistence" = db_factory()
     make_get_check(name, content, tags, db)
 
 
 @given(name=st_name, content=st_content, tags=st_tags)
-def test_delete_word(name: str, content: str, tags: list[str], db_factory: callable):
+def test_delete_word(name: str, content: str, tags: set[str], db_factory: callable):
     db: "DBPersistence" = db_factory()
     make_get_check(name, content, tags, db)
     db.delete_word(name)
@@ -49,7 +49,7 @@ def test_get_words_filtered():
 
 
 @given(names=st_name_list, content=st_content, tags=st_tags)
-def test__rename_word(names: list[str, str], content: str, tags: list[str], db_factory: callable):
+def test__rename_word(names: list[str, str], content: str, tags: set[str], db_factory: callable):
     name, new_name = names
     db: "DBPersistence" = db_factory()
     make_get_check(name, content, tags, db)
@@ -59,7 +59,7 @@ def test__rename_word(names: list[str, str], content: str, tags: list[str], db_f
 
 
 @given(name=st_name, content=st_content, tags=st_tags)
-def test__rename_word_already_exists(name: str, content: str, tags: list[str], db_factory: callable):
+def test__rename_word_already_exists(name: str, content: str, tags: set[str], db_factory: callable):
     from wordspreader.persistence import DuplicateKeyException
 
     db: "DBPersistence" = db_factory()
@@ -69,7 +69,7 @@ def test__rename_word_already_exists(name: str, content: str, tags: list[str], d
 
 
 @given(name=st_name, contents=st_content_list, tags=st_tags)
-def test__update_word(name: str, contents: list[str], tags: list[str], db_factory):
+def test__update_word(name: str, contents: list[str], tags: set[str], db_factory):
     content, new_content = contents
     db: "DBPersistence" = db_factory()
     make_get_check(name, content, tags, db)
