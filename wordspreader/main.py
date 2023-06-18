8be722c84ba76c3cb4edbd33cf551c75c3d00819
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-06-18 09:46:11 -0500

    add examples, add ability to load them

--- wordspreader/main.py
+++ wordspreader/main.py
@@ -12,6 +12,7 @@ from flet import (
     Text,
     UserControl,
 )
+from flet.utils import open_in_browser
 from flet_core import (
     BottomSheet,
     ClipBehavior,
@@ -20,6 +21,8 @@ from flet_core import (
     FloatingActionButton,
     IconButton,
     OptionalNumber,
+    PopupMenuButton,
+    PopupMenuItem,
     Ref,
     TextThemeStyle,
     colors,
@@ -42,85 +45,38 @@ from wordspreader.persistence import DBPersistence, Word
 
 # noinspection PyAttributeOutsideInit,PyUnusedLocal
 class WordSpreader(UserControl):
-    def __init__(
-        self,
-        db: DBPersistence,
-        controls: list[Control] | None = None,
-        ref: Ref | None = None,
-        width: OptionalNumber = None,
-        height: OptionalNumber = None,
-        left: OptionalNumber = None,
-        top: OptionalNumber = None,
-        right: OptionalNumber = None,
-        bottom: OptionalNumber = None,
-        expand: None | bool | int = None,
-        col: ResponsiveNumber | None = None,
-        opacity: OptionalNumber = None,
-        rotate: RotateValue = None,
-        scale: ScaleValue = None,
-        offset: OffsetValue = None,
-        aspect_ratio: OptionalNumber = None,
-        animate_opacity: AnimationValue = None,
-        animate_size: AnimationValue = None,
-        animate_position: AnimationValue = None,
-        animate_rotation: AnimationValue = None,
-        animate_scale: AnimationValue = None,
-        animate_offset: AnimationValue = None,
-        on_animation_end=None,
-        visible: bool | None = None,
-        disabled: bool | None = None,
-        data: Any = None,
-        clip_behavior: ClipBehavior | None = None,
-    ):
-        super().__init__(
-            controls,
-            ref,
-            width,
-            height,
-            left,
-            top,
-            right,
-            bottom,
-            expand,
-            col,
-            opacity,
-            rotate,
-            scale,
-            offset,
-            aspect_ratio,
-            animate_opacity,
-            animate_size,
-            animate_position,
-            animate_rotation,
-            animate_scale,
-            animate_offset,
-            on_animation_end,
-            visible,
-            disabled,
-            data,
-            clip_behavior,
-        )
-
-        def open_bs(_):
-            self.bs.open = True
-            self.bs.update()
-
-        def close_bs(_):
-            self.bs.open = False
-            self.bs.update()
-
+    def __init__(self, db: DBPersistence):
+        super().__init__()
         self.db = db
         self.word_display = WordDisplay(self.db)
         self.word_modal = WordModal(self.new_word, self.db.update_word)
         self.bs = BottomSheet(self.word_modal)
-        self.fab = FloatingActionButton(icon=icons.ADD, bgcolor=colors.BLUE, on_click=open_bs)
+        self.fab = FloatingActionButton(icon=icons.ADD, bgcolor=colors.BLUE, on_click=self.open_bs)
+        self.popup = PopupMenuButton(
+            items=[
+                PopupMenuItem(
+                    text="Show Database file in explorer",
+                    on_click=self.show_db_file_in_file_browser,
+                ),
+                PopupMenuItem(text="Load Examples", on_click=self.load_examples),
+            ],
+            tooltip="Show options",
+        )
+
+    def open_bs(self, _=None):
+        self.bs.open = True
+        self.bs.update()
+
+    def close_bs(self, _=None):
+        self.bs.open = False
+        self.bs.update()
 
     @classmethod
     def default_app_dir_db(cls):
         """Creates an instance using the default"""
         cls.default_db_path.parent.mkdir(parents=True, exist_ok=True)
         logging.info(f"Using file path `{cls.default_db_path}` for the database")
-        return cls(DBPersistence.from_file(cls.default_db_path))
+        return DBPersistence.from_file(cls.default_db_path)
 
     def did_mount(self):
         self.page.floating_action_button = self.fab
@@ -140,6 +96,7 @@ class WordSpreader(UserControl):
                 Row(
                     [
                         Text(value="Words to Spread", style=TextThemeStyle.HEADLINE_MEDIUM),
+                        self.popup,
                     ],
                     alignment=MainAxisAlignment.CENTER,
                 ),
@@ -162,9 +119,27 @@ class WordSpreader(UserControl):
 
     def new_word(self, title: str, words: str, tags: set[str] = None):
         self.db.new_word(title, words, tags)
-        self.bs.open = False
-        self.bs.update()
+        self.close_bs()
         self.update()
+
+    def show_db_file_in_file_browser(self, _):
+        open_in_browser(f"file:///{self.default_db_path.parent}")
+
+    def load_examples(self, _):
+        import yaml
+
+        with open("examples.yaml") as fp:
+            data = yaml.safe_load(fp.read())
+        for word in data.get("words"):
+            match word:
+                case {"title": title, "words": words, "tags": tags}:
+                    try:
+                        self.new_word(title, words, set(tags))
+                    except:
+                        pass
+
+
+_db = WordSpreader.default_app_dir_db()
 
 
 def main(page: Page):
@@ -172,7 +147,7 @@ def main(page: Page):
     page.horizontal_alignment = "center"
     page.scroll = "adaptive"
     # create application instance
-    app = WordSpreader.default_app_dir_db()
+    app = WordSpreader(_db)
     # add application's root control to the page
     page.add(app)
 
