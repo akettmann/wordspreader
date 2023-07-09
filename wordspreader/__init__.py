b57b272cc3527b8bcbe5655bd59b5d4a05c2b116
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-07-09 11:24:20 -0500

    handler goes here

--- wordspreader/__init__.py
+++ wordspreader/__init__.py
@@ -1,3 +1,12 @@
 # SPDX-FileCopyrightText: 2023-present mriswithe <1725647+mriswithe@users.noreply.github.com>
 #
 # SPDX-License-Identifier: MIT
+import logging
+import os
+
+log = logging.getLogger("wordspreader")
+log.addHandler(logging.NullHandler())
+if os.environ.get("WORDSPREADER_DEBUG"):
+    log.setLevel(logging.DEBUG)
+else:
+    log.setLevel(logging.WARNING)
